b784e9fdbe27f512ba151e600f862ebacde27509
Raiwin Inthasit <andaraiwin@gmail.com>
2022-09-15 14:47:15 +0700

    update

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,11 +1,60 @@
 import React from "react";
 import Reply from "./Reply";
+import { comments } from "../libs/comments";
 
 export default function Comment(props) {
+  const Profile_Comment = comments;
+
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
+      <div>
+        <div>
+          {Profile_Comment.map((x, i) => {
+            return (
+              <div key={i}>
+                <div className="d-flex gap-2 my-2">
+                  <img
+                    src={`${x.userImagePath}`}
+                    width="48"
+                    height="48"
+                    className="rounded-circle"
+                    style={{ objectFit: "cover" }}
+                  />
+                  <div
+                    className="rounded rounded-3 p-2"
+                    style={{ backgroundColor: "#3A3B3C" }}
+                  >
+                    <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+                      {x.username}
+                    </span>
+                    <br />
+                    <span style={{ color: "#E4E6EB" }}>{x.commentText}</span>
+                    <div
+                      className="d-flex align-items-center gap-1"
+                      style={{ display: x.likeNum > 0 ? "" : "none" }}
+                    >
+                      <img src="/like.svg" width={20}></img>
+                      <span style={{ color: "#B0B3B8" }}>{x.likeNum} คน</span>
+                    </div>
+                  </div>
+                </div>
+                {x.replies.map((y, idx) => {
+                  return (
+                    <Reply
+                      key={idx}
+                      username={y.username}
+                      userImagePath={y.userImagePath}
+                      replyText={y.replyText}
+                      likeNum={y.likeNum}
+                    />
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </div>
 
       {/*render Reply here... */}
     </div>
--- components/PostOwner.jsx
+++ components/PostOwner.jsx
@@ -1,5 +1,28 @@
 import React from "react";
 
 export default function PostOwner(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div className="vstack gap-3">
+        <div className="d-flex align-items-center gap-3">
+          <img
+            src="/profileImages/Myself.jpg"
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <span className="fw-semibold fs-5 text-white">
+            Raiwin Inthasit 640610665
+          </span>
+        </div>
+        <span className="text-white">ขออาจารย์สงสารนศ.ด้วยนะคับ #261207</span>
+        <div className="d-flex align-items-center gap-1">
+          <img src="/like.svg" width={20}></img>
+          <span style={{ color: "#B0B3B8" }}>18 คน</span>
+        </div>
+        <hr className="m-0 border" />
+      </div>
+    </div>
+  );
 }
